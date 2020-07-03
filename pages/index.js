94963b26739cbae1e639ad9bc6e8ba6fc9e6b22e
Curtis <CurtisWoodsArt@gmail.com>
2020-07-02 23:58:35 -0400

    Basic Index page is working

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import fetch from 'isomorphic-unfetch';
+
+
+
+const Index = (props) => {
+    return (
+        <h1>Hello! I'm the Index page.</h1>
+    );
+};
+
+// Index.getInitialProps = async () => {
+//     //Insert fetch code from Django API
+// }
+
+export default Index;
