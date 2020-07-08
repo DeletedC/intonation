503411667360ff6c48d67df78bce114be58eb096
Curtis <CurtisWoodsArt@gmail.com>
2020-07-08 01:08:39 -0400

    Moved student fetch to teacher dashboard

--- pages/index.js
+++ pages/index.js
@@ -9,26 +9,8 @@ const Index = (props) => {
     return (
         <Layout>
             <h1>Hello! I'm the Index page.</h1>
-            <ul>
-                {data?
-                (data.map((teacher, index) => {
-                    return (
-                        <li>{teacher.firstName} {teacher.lastName}</li>
-                    );
-                }))
-                : "Nothing to load."
-                }
-            </ul>
         </Layout>
     );
 };
-
-Index.getInitialProps = async () => {
-    const res = await fetch('http://localhost:8000/teachers');
-    const data = await res.json();
-    return {
-        data
-    }
-}
 
 export default Index;
---
+++ pages/teachers/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import fetch from 'isomorphic-unfetch';
+import Layout from '../../components/Layout.js';
+
+
+
+const Index = (props) => {
+    const data = props.data;
+    return (
+        <Layout>
+            <h1>Teacher Dashboard</h1>
+            <ul>
+                {data?
+                (data.map((student, index) => {
+                    return (
+                        <li>{student.firstName} {student.lastName}</li>
+                    );
+                }))
+                : "Nothing to load."
+                }
+            </ul>
+        </Layout>
+    );
+};
+
+Index.getInitialProps = async () => {
+    const res = await fetch('http://localhost:8000/students');
+    const data = await res.json();
+    return {
+        data
+    }
+}
+
+export default Index;
