5e54dbe1c0979bb23b52e2154ae0c5c06b6127c7
Curtis <CurtisWoodsArt@gmail.com>
2020-07-07 22:26:25 -0400

    Data fetching working

--- components/Layout.js
+++ components/Layout.js
@@ -5,8 +5,12 @@ const Layout = (props) => {
     return (
         <>
             <Head>
-            {/* Materialize */}
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"/>
+                {/* Google Icon Font */}
+                <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
+                {/* Materialize CSS */}
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"/>
+                {/* Materialize JS */}
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js" defer/>
                 <title>Intonation</title>
             </Head>
             <div>
--- pages/index.js
+++ pages/index.js
@@ -5,15 +5,30 @@ import Layout from '../components/Layout';
 
 
 const Index = (props) => {
+    const data = props.data;
     return (
         <Layout>
             <h1>Hello! I'm the Index page.</h1>
+            <ul>
+                {data?
+                (data.map((teacher, index) => {
+                    return (
+                        <li>{teacher.firstName} {teacher.lastName}</li>
+                    );
+                }))
+                : "Nothing to load."
+                }
+            </ul>
         </Layout>
     );
 };
 
-// Index.getInitialProps = async () => {
-//     //Insert fetch code from Django API
-// }
+Index.getInitialProps = async () => {
+    const res = await fetch('http://localhost:8000/teachers');
+    const data = await res.json();
+    return {
+        data
+    }
+}
 
 export default Index;
